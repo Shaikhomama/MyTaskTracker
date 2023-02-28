3a4877187f0ad06673d96cd1b66a2c440b7d77f2
Omama Shaikh <omamashaikh50@gmail.com>
2023-02-28 17:59:39 -0500

    activity_main.xml add

--- app/src/main/java/com/example/mytaskstracker/MainActivity.java
+++ app/src/main/java/com/example/mytaskstracker/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.mytaskstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.SaveDateListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initListButton();
         initSettingsButton();
+        initToggleButton();
+        setForEditing(false);
+        initChangeDateButton();
     }
 
     private void initListButton(){                                   //connects to ContactListActivity
@@ -29,12 +40,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initSettingsButton(){                              //connects to ContactSettingsActivity
-        ImageButton ibList = findViewById(R.id.imageButtonSettings);
-        ibList.setOnClickListener(new View.OnClickListener() {
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+        ibSettings.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
                 Intent intent = new Intent(MainActivity.this, TaskSettingsActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void initToggleButton(){
+        final ToggleButton editToggle = (ToggleButton) findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setForEditing(editToggle.isChecked());
+            }
+        });
+    }
+
+    private void setForEditing(boolean enabled){
+        EditText editTask = findViewById(R.id.editTask);
+        EditText editDescription = findViewById(R.id.editTextDescription);
+        Button saveButton = findViewById(R.id.buttonSave);
+        Button changeButton = findViewById(R.id.buttonChange);
+
+        editTask.setEnabled(enabled);
+        editDescription.setEnabled(enabled);
+        saveButton.setEnabled(enabled);
+        changeButton.setEnabled(enabled);
+    }
+
+    @Override
+    public void didFinishDatePickerDialog(Calendar selectedTime) {
+        TextView dueDate = findViewById(R.id.textDueDate);
+        dueDate.setText(DateFormat.format("MM/dd/yyyy",selectedTime));
+    }
+
+    private void initChangeDateButton(){
+        Button changeDate = findViewById(R.id.buttonChange);
+        changeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                DatePickerDialog datePickerDialog = new DatePickerDialog();
+                datePickerDialog.show(fm, "DatePick");
             }
         });
     }
