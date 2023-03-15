a1249773a6da7aadec4b04ae24ee7629f968f1f3
Omama Shaikh <omamashaikh50@gmail.com>
2023-03-15 19:22:14 -0400

    Fixed minor things & upgraded build

--- app/src/main/java/com/example/mytaskstracker/TaskSettingsActivity.java
+++ app/src/main/java/com/example/mytaskstracker/TaskSettingsActivity.java
@@ -19,6 +19,7 @@ public class TaskSettingsActivity extends AppCompatActivity {
 
         initListButton();
         initSettingsButton();
+        initSettings();
         initSortByClick();
         initSortOrderClick();
     }
